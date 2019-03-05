196765104e0b3ff3db3c92561e6d5436d696ffbc
Andrey <andrei-fom@mail.ru>
2019-03-05 23:00:02 +0300

    1 Task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -5,5 +5,13 @@
  * @returns {number}
  */
 function pow (m, n) {
-
+    function  pow(x,n) {
+        if(n!=1){
+            return x*pow(x,n-1);
+        } else{
+            return x;
+        }
+    }
+    alert(pow(2,3));
+    alert(pow(3,4));
 }
